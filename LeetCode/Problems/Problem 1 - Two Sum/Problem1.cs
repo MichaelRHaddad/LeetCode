71e36b2efa88a3558a559369d0d3fc8ffd720fa0
Michael <sipocomp@gmail.com>
2021-05-28 17:51:13 +0300

    Problem 1 (Two Sum) solution

---
+++ LeetCode/Problems/Problem 1 - Two Sum/Problem1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems.Problem1_TwoSum
+{
+    class Problem1
+    {
+        // Accepted by LeetCode
+        // Solves problem 1 in 232 ms, faster then 93.36% of C# submissions
+
+        public int[] TwoSum(int[] nums, int target)
+        {
+            var numberToIndexMap = new Dictionary<int, ushort>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(numberToIndexMap.ContainsKey(target - nums[i]))
+                {
+                    return new int[2] { i, numberToIndexMap[target - nums[i]] };
+                }
+
+                numberToIndexMap.Add(nums[i], (ushort)i);
+            }
+
+            throw new ArgumentException("No Solution");
+        }
+    }
+}
---
+++ LeetCode/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var problem = new Problems.Problem1_TwoSum.Problem1();
+
+            var solution = problem.TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            Console.WriteLine($"[{solution[0]}, {solution[1]}]");
+        }
+    }
+}
