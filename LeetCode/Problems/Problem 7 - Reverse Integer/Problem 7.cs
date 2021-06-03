f82c963b4e06397b2d4ffe6bc5374274448391b1
Michael <sipocomp@gmail.com>
2021-06-04 02:13:59 +0300

    Problem 7 (Reverse Integer) solution

--- LeetCode/Problems/Problem 7 - Reverse Integer/Problem 7.cs
+++ LeetCode/Problems/Problem 7 - Reverse Integer/Problem 7.cs
@@ -4,25 +4,26 @@ namespace LeetCode.Problems.Problem7_ReverseInteger
 {
     class Problem7
     {
+        // Accepted by LeetCode
+        // Solves problem 7 in 40 ms, faster then 81.73% of C# submissions
+
         public int Reverse(int x)
         {
-            int y = Math.Abs(x);
-            if (y < 10)
+            int result = 0;
+            while (x != 0)
             {
-                return x;
+                int lastDigit = x % 10; // Could be negative
+                if (lastDigit >= 0 && result > (int.MaxValue - lastDigit) / 10 ||
+                    lastDigit <= 0 && result < (int.MinValue - lastDigit) / 10)
+                {
+                    return 0;
+                }
+
+                result = result * 10 + lastDigit;
+                x /= 10;
             }
 
-            int lastDigit = y % 10;
-            int yLength = (int)Math.Log10(y);
-            int nextDigitsReversed = Reverse(y / 10);
-            int absoluteResult = lastDigit * (int)Math.Pow(10, yLength) + nextDigitsReversed;
-
-            if (x != y)
-            {
-                absoluteResult *= -1;
-            }
-            
-            return absoluteResult;
+            return result;
         }
     }
 }
