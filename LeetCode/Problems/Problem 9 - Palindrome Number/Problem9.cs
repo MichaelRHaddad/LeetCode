ca8a2c53b00a098364906a3a7d0de2c1e29c9a9a
Michael <sipocomp@gmail.com>
2021-06-04 13:10:35 +0300

    Create files for problem 9

---
+++ LeetCode/Problems/Problem 9 - Palindrome Number/Problem9.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems.Problem9_PalindromeNumber
+{
+    partial class Problem9
+    {
+        public int[] bool IsPalindrome(int x)
+        {
+            return Solution1(x);
+        }
+    }
+}
---
+++ LeetCode/Problems/Problem 9 - Palindrome Number/Solution 1.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems.Problem9_PalindromeNumber
+{
+    partial class Problem9
+    {
+        private bool Solution1(int x)
+        {
+
+        }
+    }
+}
