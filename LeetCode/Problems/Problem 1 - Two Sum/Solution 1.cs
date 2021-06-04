f493ca7ee87e5bf3f519518c30283f4d3be43a51
Michael <sipocomp@gmail.com>
2021-06-04 13:07:01 +0300

    Separating solutions into own files

---
+++ LeetCode/Problems/Problem 1 - Two Sum/Solution 1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCode.Problems.Problem1_TwoSum
+{
+    partial class Problem1
+    {
+        /**
+         * Accepted by LeetCode.
+         * Solves problem 1 faster then 93.36% of C# submissions.
+         * Memory usage better than 16.21% of C# subissions.
+         **/
+        private int[] Solution1(int[] nums, int target)
+        {
+            var numberToIndexMap = new Dictionary<int, ushort>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (numberToIndexMap.ContainsKey(target - nums[i]))
+                {
+                    return new int[2] { i, numberToIndexMap[target - nums[i]] };
+                }
+
+                numberToIndexMap.Add(nums[i], (ushort)i);
+            }
+
+            throw new ArgumentException("No Solution");
+        }
+    }
+}
---
+++ LeetCode/Problems/Problem 7 - Reverse Integer/Solution 1.cs
@@ -0,0 +1,29 @@
+﻿namespace LeetCode.Problems.Problem7_ReverseInteger
+{
+    partial class Problem7
+    {
+        /**
+         * Accepted by LeetCode.
+         * Solves problem 7 faster then 81.87% of C# submissions.
+         * Memory usage better than 82.83% of C# subissions.
+         **/
+        private int Solution1(int x)
+        {
+            int result = 0;
+            while (x != 0)
+            {
+                int lastDigit = x % 10; // Could be negative
+                if (lastDigit >= 0 && result > (int.MaxValue - lastDigit) / 10 ||
+                    lastDigit <= 0 && result < (int.MinValue - lastDigit) / 10)
+                {
+                    return 0;
+                }
+
+                result = result * 10 + lastDigit;
+                x /= 10;
+            }
+
+            return result;
+        }
+    }
+}
