459a8ac7cec0a44a9803df09bc38d49bb42527b6
Michael <sipocomp@gmail.com>
2021-06-04 13:15:50 +0300

    First draft of problem 9

--- LeetCode/Problems/Problem 9 - Palindrome Number/Solution 1.cs
+++ LeetCode/Problems/Problem 9 - Palindrome Number/Solution 1.cs
@@ -4,7 +4,16 @@
     {
         private bool Solution1(int x)
         {
+            string number = x.ToString();
+            for(int i = 0; i < number.Length / 2; i++)
+            {
+                if(number[i] != number[number.Length - i])
+                {
+                    return false;
+                }
+            }
 
+            return true;
         }
     }
 }
