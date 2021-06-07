a6e83a40de9b4af2174df4503e4f51a600bde5f5
Michael <sipocomp@gmail.com>
2021-06-08 00:28:34 +0300

    First draft of Problem 9 solution 2

--- LeetCode/Problems/Problem 9 - Palindrome Number/Solution 2.cs
+++ LeetCode/Problems/Problem 9 - Palindrome Number/Solution 2.cs
@@ -1,10 +1,29 @@
-﻿namespace LeetCode.Problems.Problem9_PalindromeNumber
+﻿using System;
+
+namespace LeetCode.Problems.Problem9_PalindromeNumber
 {
     partial class Problem9
     {
         private bool Solution2(int x)
         {
-            
+            if (x < 0) return false;
+
+            while (x != 0)
+            {
+                byte length = (byte)Math.Log10(x);
+                byte firstDigit = (byte)(x / Math.Pow(10, length));
+                byte lastDigit = (byte)(x % 10);
+
+                if (firstDigit != lastDigit)
+                {
+                    return false;
+                }
+
+                x -= firstDigit * (int)Math.Pow(10, length); // Removing first digit
+                x /= 10; // Removing last digit
+            }
+
+            return true;
         }
     }
 }
