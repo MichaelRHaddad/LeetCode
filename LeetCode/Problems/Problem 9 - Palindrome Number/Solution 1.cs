7d068e5d0ccd14e35b6d392ee07dac6f511d868c
Michael <sipocomp@gmail.com>
2021-06-07 23:35:52 +0300

    Problem 9 (Palindrome Number) solution #1

--- LeetCode/Problems/Problem 9 - Palindrome Number/Solution 1.cs
+++ LeetCode/Problems/Problem 9 - Palindrome Number/Solution 1.cs
@@ -2,12 +2,19 @@
 {
     partial class Problem9
     {
+        /**
+         * Accepted by LeetCode.
+         * Solves problem 9 faster then 38.17% of C# submissions.
+         * Memory usage better than 35.49% of C# subissions.
+         **/
         private bool Solution1(int x)
         {
+            if (x < 0) return false;
+
             string number = x.ToString();
             for(int i = 0; i < number.Length / 2; i++)
             {
-                if(number[i] != number[number.Length - i])
+                if(number[i] != number[number.Length - 1 - i])
                 {
                     return false;
                 }
