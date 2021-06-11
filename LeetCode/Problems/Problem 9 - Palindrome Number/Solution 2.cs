edf3aeea68acc26661c8ef88eea6ab04fef43054
Michael <sipocomp@gmail.com>
2021-06-11 15:32:32 +0300

    Second draft of problem 9 solution 2

--- LeetCode/Problems/Problem 9 - Palindrome Number/Solution 2.cs
+++ LeetCode/Problems/Problem 9 - Palindrome Number/Solution 2.cs
@@ -8,12 +8,42 @@ namespace LeetCode.Problems.Problem9_PalindromeNumber
         {
             if (x < 0) return false;
 
-            while (x != 0)
+            byte leadingZeros = 0;
+            while (x >= 10)
             {
+                // ()100121001
+                // (00)12100
+                // (0)1210
+                // ()121
+                // ()2
+
                 byte orderOfMagnitude = (byte)Math.Log10(x);
+                // 8
+                // 4
+                // 3
+                // 2
+                // 0
+
                 int biggestWeight = (int)Math.Pow(10, orderOfMagnitude);
-                byte firstDigit = (byte)(x / biggestWeight);
+                // 100000000
+                // 10000
+                // 1000
+                // 100
+                // 1
+
+                byte firstDigit = leadingZeros == 0 ? (byte)(x / biggestWeight) : (byte)0;
+                // 1
+                // 0
+                // 0
+                // 1
+                // 2
+
                 byte lastDigit = (byte)(x % 10);
+                // 1
+                // 0
+                // 0
+                // 1
+                // 2
 
                 if (firstDigit != lastDigit)
                 {
@@ -21,10 +51,34 @@ namespace LeetCode.Problems.Problem9_PalindromeNumber
                 }
 
                 // Removing first digit
-                x -= firstDigit * biggestWeight;
+                if (leadingZeros > 0)
+                {
+                    leadingZeros--;
+                    // (0)12100
+                    // ()1210
+                }
+                else
+                {
+                    x -= firstDigit * biggestWeight;
+                    // ()121001
+                    // ()21
+                    // ()0
+
+                    byte newOrderOfMagnitude = (byte)Math.Log10(x);
+                    // 5
+                    // 1
+
+                    leadingZeros = (byte)(orderOfMagnitude - 1 - newOrderOfMagnitude);
+                    // 2
+                    // 0
+                }
 
                 // Removing last digit
                 x /= 10;
+                // (00)12100
+                // (0)1210
+                // ()121
+                // ()2
             }
 
             return true;
