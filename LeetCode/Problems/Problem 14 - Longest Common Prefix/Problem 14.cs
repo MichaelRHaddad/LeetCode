e92220d6752c55aa562b71d12443aa6eb4f17059
Michael <sipocomp@gmail.com>
2021-06-16 13:53:51 +0300

    Adding initial files for problem 14

---
+++ LeetCode/Problems/Problem 14 - Longest Common Prefix/Problem 14.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems.Problem14_LongestCommonPrefix
+{
+    partial class Problem14
+    {
+        public string LongestCommonPrefix(string[] strs)
+        {
+            return Solution1(strs);
+        }
+    }
+}
---
+++ LeetCode/Problems/Problem 14 - Longest Common Prefix/Solution 1.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems.Problem14_LongestCommonPrefix
+{
+    partial class Problem14
+    {
+        private string Solution1(string[] strs)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
