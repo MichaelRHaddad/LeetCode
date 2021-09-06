2a744a0fab1f15aac811c5d55f1d9bfe8da88ebb
Michael <sipocomp@gmail.com>
2021-09-06 12:52:00 +0300

    Adding initial files for problem 2

---
+++ LeetCode/Problems/Problem 2 - Add Two Numbers/Problem 2.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems.Problem2_AddTwoNumbers
+{
+    partial class Problem2
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            return Solution1(l1, l2);
+        }
+    }
+}
---
+++ LeetCode/Problems/Problem 2 - Add Two Numbers/Solution 1.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems.Problem2_AddTwoNumbers
+{
+    partial class Problem2
+    {
+        private ListNode Solution1(ListNode l1, ListNode l2)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
