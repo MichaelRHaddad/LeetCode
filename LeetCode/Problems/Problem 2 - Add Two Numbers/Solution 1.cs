377d71f48151a86c7936a277f45a7f5b9da15c09
Michael <sipocomp@gmail.com>
2021-09-10 17:49:59 +0300

    Problem 2, solution 1, 1st draft

--- LeetCode/Problems/Problem 2 - Add Two Numbers/Solution 1.cs
+++ LeetCode/Problems/Problem 2 - Add Two Numbers/Solution 1.cs
@@ -4,7 +4,27 @@
     {
         private ListNode Solution1(ListNode l1, ListNode l2)
         {
-            throw new System.NotImplementedException();
+            return AddTwoNumbers(l1, l2, 0);
+        }
+
+        private ListNode AddTwoNumbers(ListNode l1, ListNode l2, int carry)
+        {
+            int value;
+            ListNode next;
+
+            if (l1.next == null)
+            {
+                next = l2.next;
+            }
+            if (l2.next == null)
+            {
+                next = l1.next;
+            }
+
+            value = (l1.val + l2.val) % 10;
+            next = AddTwoNumbers(l1.next, l2.next, (l1.val + l2.val) / 10);
+
+            return new ListNode(value, next);
         }
     }
 }
