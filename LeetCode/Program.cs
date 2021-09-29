e74ef9c5b2121f14819596e61dca48f96936b7f7
Michael <sipocomp@gmail.com>
2021-09-29 15:31:32 +0300

    Some playground stuff

--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -10,7 +10,7 @@ namespace LeetCode
             var problem = new Problem2();
 
             var l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
-            var l2 = new ListNode(5, new ListNode(6, new ListNode(7)));
+            var l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
 
             var result = problem.AddTwoNumbers(l1, l2);
 
