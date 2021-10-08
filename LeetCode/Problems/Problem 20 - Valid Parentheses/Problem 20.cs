fd9adb9b2fa13c8a4a72b79f554aca75cf92ed67
Michael <sipocomp@gmail.com>
2021-10-08 16:51:23 +0300

    Adding initial files

---
+++ LeetCode/Problems/Problem 20 - Valid Parentheses/Problem 20.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems.Problem20_ValidParentheses
+{
+    partial class Problem20
+    {
+        public string ValidParentheses(string s)
+        {
+            return Solution1(s);
+        }
+    }
+}
---
+++ LeetCode/Problems/Problem 20 - Valid Parentheses/Solution 1.cs
@@ -0,0 +1,10 @@
+﻿namespace LeetCode.Problems.Problem20_ValidParentheses
+{
+    partial class Problem20
+    {
+        private string Solution1(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
